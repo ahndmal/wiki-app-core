32a523908f1dde20ebe37cbd3b09c929ed0e83a3
andriim <quadr988@gmail.com>
2023-08-29 18:50:59 +0300

    3.1.3

--- src/main/java/com/anma/conflappcore/contr/SpaceController.java
+++ src/main/java/com/anma/conflappcore/contr/SpaceController.java
@@ -1,6 +1,9 @@
 package com.anma.conflappcore.contr;
 
+import com.anma.conflappcore.repo.PageRepo;
 import com.anma.conflappcore.repo.SpaceRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/spaces")
 public class SpaceController {
+    private final Logger LOG = LoggerFactory.getLogger(SpaceController.class);
     private final SpaceRepo spaceRepo;
+    private final PageRepo pageRepo;
 
     @Autowired
-    public SpaceController(SpaceRepo spaceRepo) {
+    public SpaceController(SpaceRepo spaceRepo, PageRepo pageRepo) {
         this.spaceRepo = spaceRepo;
+        this.pageRepo = pageRepo;
     }
 
     @GetMapping
@@ -27,6 +33,12 @@ public class SpaceController {
     @GetMapping("/{key}")
     public String getSpace(@PathVariable String key, Model model) {
         model.addAttribute("space", spaceRepo.findBySpaceKey(key));
+
+        var spacePages = pageRepo.findBySpaceKeyQuery(key);
+
+        LOG.info(">>> SPace page: serving " + spacePages.size() + " pages ");
+
+        model.addAttribute("spacePages", spacePages);
         return "space/space";
     }
 
--- src/main/java/com/anma/conflappcore/repo/PageRepo.java
+++ src/main/java/com/anma/conflappcore/repo/PageRepo.java
@@ -13,8 +13,11 @@ public interface PageRepo extends JpaRepository<WikiPage, Long> {
     @Query(value = "SELECT * FROM pages p WHERE p.title = ?1", nativeQuery = true)
     public List<WikiPage> findByTitle(String pageTitle);
 
-    @Query(value = "select * from pages p where p.las", nativeQuery = true)
-    List<WikiPage> getLastUpdated();
+//    @Query(value = "select * from pages p where p.last_updated ", nativeQuery = true)
+//    List<WikiPage> findByLastUpdated();
 
     public List<WikiPage> findBySpaceKey(String key);
+
+    @Query(value = "select * from pages p where p.space_key = ?1", nativeQuery = true)
+    public List<WikiPage> findBySpaceKeyQuery(String key);
 }
